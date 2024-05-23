4f8f0ae945aaf392917b8228aff2329e7134a63e
An Phan <phanan1309@gmail.com>
2024-05-23 14:26:58 +0700

    fixed some issues

--- tf-jenkins/jenkins_scripts.sh
+++ tf-jenkins/jenkins_scripts.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+
+# Install self-hosted runner
+# Create a folder
+mkdir actions-runner && cd actions-runner
+# Download the latest runner package
+url -o actions-runner-linux-x64-2.316.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.316.1/actions-runner-linux-x64-2.316.1.tar.gz
+echo "d62de2400eeeacd195db91e2ff011bfb646cd5d85545e81d8f78c436183e09a8  actions-runner-linux-x64-2.316.1.tar.gz" | shasum -a 256 -c# Extract the installer
+tar xzf ./actions-runner-linux-x64-2.316.1.tar.gz
+cd ~
+
+
 # Install Jenkins
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install fontconfig openjdk-17-jre
-sudo apt-get install jenkins
+# sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+#   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+# echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+#   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+#   /etc/apt/sources.list.d/jenkins.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install fontconfig openjdk-17-jre
+# sudo apt-get install jenkins
+
+# Install Node
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install Zip and Unzip
+sudo apt install -y zip
+sudo apt install -y unzip
 
 # Install git
 sudo apt-get install git -y
